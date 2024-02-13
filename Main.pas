7a035a50addba950273ac4ccbf3599997cf750e5
Mike Brashler <mike.brashler@gmail.com>
2024-02-13 00:48:44 -0800

    roll version number to v1.84-pr3

--- Main.pas
+++ Main.pas
@@ -21,7 +21,7 @@ uses
 
 const
   WM_TBDOWN = WM_USER+1;
-  sVersion: String = '1.84-pr2';  { Sets version strings in UI panel. }
+  sVersion: String = '1.84-pr3';  { Sets version strings in UI panel. }
 
 type
 
