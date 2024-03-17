d7890faf5b9e734b66cc5affe9cbbb1da79cafe6
Mike Brashler <mike.brashler@gmail.com>
2024-03-17 13:26:07 -0700

    Roll version number for candidate build

--- Main.pas
+++ Main.pas
@@ -21,7 +21,7 @@ uses
 
 const
   WM_TBDOWN = WM_USER+1;
-  sVersion: String = '1.84';  { Sets version strings in UI panel. }
+  sVersion: String = '1.84-rc4';  { Sets version strings in UI panel. }
 
 type
 
