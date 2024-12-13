dbf9dc4e0867ff9e8a7b2dd8610a30726b546530
Mike Brashler <mike.brashler@gmail.com>
2024-12-12 23:42:56 -0800

    Send 'TU MyCall' after three consecutive QSOs
    - 'TI <my>' is now sent after three consecutive QSOS.
    - Sending CQ will reset this counter.
    - The goal is to mimic good operating practice of sending
      Station ID after three consecutive QSOs when running.
    - Does not apply to HST Competition.

--- Main.pas
+++ Main.pas
@@ -322,6 +322,7 @@ type
     UserExchangeDirty: boolean; // SetMyExchange is called after exchange edits
     CWSpeedDirty: boolean;      // SetWpm is called after CW Speed edits
     RitLocal: integer;          // tracks incremented RIT Value
+    QsoCountSinceStationID: Integer;  // send station ID once-every-three QSOs
     function CreateContest(AContestId : TSimContest) : TContest;
     procedure ConfigureExchangeFields;
     procedure SetMyExch1(const AExchType: TExchange1Type; const Avalue: string);
@@ -369,7 +370,6 @@ type
     procedure UpdCWMaxRxSpeed(Maxspd: integer);
     procedure ClientHTTP1Redirect(Sender: TObject; var dest: string;
       var NumRedirect: Integer; var Handled: Boolean; var VMethod: string);
-
   end;
 
 function ToStr(const val : TExchange1Type): string; overload;
@@ -442,6 +442,7 @@ begin
 
   UserCallsignDirty := False;
   UserExchangeDirty := False;
+  QsoCountSinceStationID := 0;
 
   // load DXCC support
   gDXCCList := TDXCC.Create;
@@ -547,6 +548,21 @@ begin
   if AMsg = msgNR then
     NrSent := true;
   Tst.Me.SendMsg(AMsg);
+
+  // send station ID once-every-three QSOs
+  if RunMode <> rmHST then
+    case AMsg of
+    msgCQ: QsoCountSinceStationID := 0;
+    msgTU:
+      begin
+        Inc(QsoCountSinceStationID);
+        if QsoCountSinceStationID >= 3 then
+          begin
+            SendMsg(msgMyCall);
+            QsoCountSinceStationID := 0;
+          end;
+      end;
+    end;
 end;
 
 
